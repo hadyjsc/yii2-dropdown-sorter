c04ffec56c444ce6aad88230feb26abd17854420
Hady Eka Saputra <hadyekasaputra@gmail.com>
2020-01-30 12:59:43 +0700

    change class name

--- ButtonDropdownSorter.php
+++ ButtonDropdownSorter.php
@@ -3,7 +3,7 @@
  * @see http://www.yiiframework.com/doc-2.0/yii-widgets-linksorter.html
  */
 
-namespace jscdev\bs4;
+namespace jscdev\yii2;
 
 use yii\base\InvalidConfigException;
 use yii\base\Widget;
